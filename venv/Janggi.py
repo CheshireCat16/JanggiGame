e43543a4a08f4c374874953153154e2af5404fb8
CheshireCat16 <john.cheshire@gmail.com>
2021-03-20 14:58:12 -0230

    Starting setting up pygame

---
+++ venv/Janggi.py
@@ -0,0 +1,11 @@
+import pygame
+from pygame.locals import *
+
+class Janggi:
+    def on_execute(self):
+        pass
+
+
+if __name__ == "__main__":
+    JanggiApp = Janggi()
+    JanggiApp.on_execute()
